d22bd82cdcde2c3b685f35b2d31e5512de804d34
aminst <aminst79@gmail.com>
2021-04-03 13:26:24 +0430

    Add get_project_info

--- server/includes/Project.h
+++ server/includes/Project.h
@@ -1,11 +1,23 @@
 #ifndef PROJECT_H
 #define PROJECT_H
 
+#include <stdio.h>
+#include <string.h>
+
+#include "Utils.h"
+
+#define WAITING_LIST_LIMIT 5
+#define EMPTY_WAITING_LIST_VALUE -1
+
 typedef struct
 {
     int id;
     char* name;
-    int waiting_list[4];
+    int waiting_list[WAITING_LIST_LIMIT-1];
+    int sold;
 } Project;
+
+char* get_project_info(Project project);
+char* get_waiting_list_status(Project project);
 
 #endif
--- server/src/Project.c
+++ server/src/Project.c
@@ -1,3 +1,39 @@
 #include "Project.h"
 
+char* get_waiting_list_status(Project project)
+{
+    int count = 0;
+    for (int i = 0; i < WAITING_LIST_LIMIT; i++)
+    {
+        if(project.waiting_list[i] != EMPTY_WAITING_LIST_VALUE)
+            count++;
+    }
 
+    char* status = (char*)malloc(50);
+
+    if (count == WAITING_LIST_LIMIT)
+        return "FULL";
+    else
+    {
+        char* status = (char*)malloc(50);
+        int remaining = WAITING_LIST_LIMIT - count;
+        strcat(status, int_to_str(remaining));
+        strcat(status, " Remaining");
+        return status;
+    }
+}
+
+char* get_project_info(Project project)
+{
+    char* project_info = (char*)malloc(1000);
+    strcat(project_info, "Project ID: ");
+    strcat(project_info, int_to_str(project.id));
+    strcat(project_info, "|");
+    strcat(project_info, "Project Name: ");
+    strcat(project_info, project.name);
+    strcat(project_info, "|");
+    strcat(project_info, "Waiting List Status: ");
+    strcat(project_info, get_waiting_list_status(project));
+    strcat(project_info, "\n");
+    return project_info;
+}
